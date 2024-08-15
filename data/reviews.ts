5cc910e222539013dc6293754e562c9d4d94c128
Muhammad Osama <madebyosama@gmail.com>
2024-08-15 17:07:25 +0500

    Updated

--- data/reviews.ts
+++ data/reviews.ts
@@ -1,6 +1,8 @@
 import SophiaHanvold from '@/assets/images/clients/sophiahanvold.png';
 import MichelleCanton from '@/assets/images/clients/michellecanton.png';
 import AkmalQureshi from '@/assets/images/clients/akmalqureshi.png';
+import AdeyinkaAdegoke from '@/assets/images/clients/adeyinkaadegoke.webp';
+import AlexAllegu from '@/assets/images/clients/alexallegu.jpg';
 
 const reviews = [
   {
@@ -22,19 +24,20 @@ const reviews = [
     authorWebsite: 'solverscave.com',
   },
   {
-    description: `Working with Muhammad Osama was great. He was accessible and extremely collaborative on this project. He completed all milestones quickly and ahead of schedule.`,
-    authorImage: AkmalQureshi,
-    authorName: 'Akmal Qureshi',
-    authorWebsite: 'solverscave.com',
+    description: `Osama (Mohamed) has been exceptional and always available for changes. He even helped on additional scope. He managed to customize every single aspect of my website, always with a positive attitude.`,
+    authorImage: AlexAllegu,
+    authorName: 'Alex Allegu',
+    authorWebsite: 'hiddentrackafrica.com',
   },
   {
-    description: `Working with Muhammad Osama was great. He was accessible and extremely collaborative on this project. He completed all milestones quickly and ahead of schedule.`,
-    authorImage: AkmalQureshi,
-    authorName: 'Akmal Qureshi',
-    authorWebsite: 'solverscave.com',
+    description: `Awesome developer. Always willing to work with you and very professional. Available to make changes and patient. He understands the job very well and he is highly skilled. We have been doing business for over 5 years.`,
+    authorImage: AdeyinkaAdegoke,
+    authorName: 'Adeyinka Adegoke',
+    authorWebsite: 'melaninpeople.com',
   },
+
   {
-    description: `Working with Muhammad Osama was great. He was accessible and extremely collaborative on this project. He completed all milestones quickly and ahead of schedule.`,
+    description: `Professional and very friendly with high knowledge on many fields , he have recommendations for your project and he don't harry to complete the order but he give you other ideas how to improve your work. `,
     authorImage: AkmalQureshi,
     authorName: 'Akmal Qureshi',
     authorWebsite: 'solverscave.com',
