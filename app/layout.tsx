2ce065d0e564d7ab3fb499e94f747c92ce626349
Muhammad Osama <madebyosama@gmail.com>
2024-09-30 14:19:00 +0500

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import localFont from 'next/font/local';
 import Nav from '@/components/nav/Nav';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import { GoogleAnalytics } from '@next/third-parties/google';
-import Script from 'next/script';
 import './global.css';
 
 import Footer from '@/components/footer/Footer';
@@ -37,7 +35,11 @@ export default function RootLayout({
     <html className={satoshi.className}>
       <head>
         <meta name='robots' content='noindex'></meta>
-
+        <script
+          defer
+          src='https://cloud.umami.is/script.js'
+          data-website-id='9fb6a204-44d4-404a-a889-31ef94fe7b94'
+        ></script>
         <link
           href='/assets/images/favicon-light.png'
           rel='icon'
@@ -74,18 +76,7 @@ export default function RootLayout({
         <div className='overlay'></div>
         <Nav />
         <main>{children}</main>
-        <GoogleAnalytics gaId='G-X1SBW0TW14' />
         <SpeedInsights />
-        <Script id='show-banner'>
-          {` (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:5125309,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
-        </Script>
         <Footer />
       </body>
     </html>
