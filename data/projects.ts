582add097d120600a60f7218e37e7e28fea2da0f
Muhammad Osama <madebyosama@gmail.com>
2025-01-07 20:47:25 +0500

    Updated

--- data/projects.ts
+++ data/projects.ts
@@ -1,8 +1,4 @@
-import VenomSecuirtyImage from '@/assets/images/projects/venomsecurity.jpg';
-import RealMesaPetFoodImage from '@/assets/images/projects/realmesapetfood.jpg';
 import KryptlocImage from '@/assets/images/projects/kryptloc.png';
-import MedicosurveillanceImage from '@/assets/images/projects/medicosurveillance.jpg';
-import CasaFenixImage from '@/assets/images/projects/fenixsayulita.jpg';
 import SaeedVisaConsultantsImage from '@/assets/images/projects/saeedvisaconsultants.jpg';
 import HiddenTrackAfricaImage from '@/assets/images/projects/hiddentrackafrica.jpg';
 
@@ -23,44 +19,13 @@ const Projects = [
     url: 'https://hiddentrackafrica.com/',
     build: 'WordPress',
   },
-  {
-    image: VenomSecuirtyImage,
-    title: 'Venom Security',
-    description: 'Security',
-    link: '/projects/venomsecurity',
-    url: 'https://venomsecurity.ca',
-    build: 'WordPress',
-  },
-  {
-    image: RealMesaPetFoodImage,
-    title: 'Real Mesa Pet Food',
-    description: 'Pet Food',
-    link: '/projects/gou-oui.com',
-    url: 'https://realmesapetfood.com/',
-    build: 'WordPress',
-  },
+
   {
     image: KryptlocImage,
     title: 'Kryptloc',
     description: 'Digital Marketplace',
     link: '/projects/kryptloc',
     url: 'https://kryptloc.com',
-    build: 'WordPress',
-  },
-  {
-    image: MedicosurveillanceImage,
-    title: 'Medicosurveillance',
-    description: 'Health / Medicine',
-    link: '/projects/medicosurveillance',
-    url: 'https://medicosurveillance.com.au',
-    build: 'WordPress',
-  },
-  {
-    image: CasaFenixImage,
-    title: 'Casa Fenix',
-    description: 'AirBnb / Rental',
-    link: '/projects/medicosurveillance',
-    url: 'https://fenixsayulita.com/',
     build: 'WordPress',
   },
 ];
