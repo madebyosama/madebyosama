943d112fc1b4f9ec36c77d01bf7deae523b9d642
Muhammad Osama <madebyosama@gmail.com>
2025-04-12 15:42:05 +0500

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,83 +1,32 @@
-import localFont from 'next/font/local';
-import Nav from '@/components/nav/Nav';
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import './global.css';
+import type { Metadata } from 'next';
+import { Bricolage_Grotesque } from 'next/font/google';
+import { Outfit } from 'next/font/google';
+import './globals.css';
 
-import Footer from '@/components/footer/Footer';
-import { Metadata } from 'next';
-export const metadata: Metadata = {
-  title: 'Muhammad Osama',
-  description: 'i build websites',
-  keywords: ['website designer'],
-};
-
-const satoshi = localFont({
-  src: [
-    {
-      path: '../assets/fonts/Satoshi-Medium.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/Satoshi-Black.woff2',
-      weight: '700',
-      style: 'normal',
-    },
-  ],
+const bricolageGrotesque = Bricolage_Grotesque({
+  variable: '--font-bricolage-grotesque',
+  subsets: ['latin'],
 });
+
+const outfit = Outfit({
+  variable: '--font-outfit',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Muhammad Osama - Website Designer & Developer',
+  description: 'Website Designer & Developer',
+};
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html className={satoshi.className}>
-      <head>
-        <meta name='robots' content='noindex'></meta>
-        <script
-          defer
-          src='https://cloud.umami.is/script.js'
-          data-website-id='9fb6a204-44d4-404a-a889-31ef94fe7b94'
-        ></script>
-        <link
-          href='/assets/images/favicon-light.png'
-          rel='icon'
-          media='(prefers-color-scheme: light)'
-        />
-        <link
-          href='/assets/images/favicon-dark.png'
-          rel='icon'
-          media='(prefers-color-scheme: dark)'
-        />
-        <meta
-          property='og:image'
-          content='https://firebasestorage.googleapis.com/v0/b/uploadedbyosama.appspot.com/o/Thumbnails%2FWebsite%2Fwebsite_thumbnail.jpg?alt=media&token=b6b33f55-3de9-4417-a5b9-bfbdcc4734e2'
-        />
-        <meta property='og:url' content='https://madebyosama.com' />
-        <meta property='og:type' content='website' />
-        <meta property='og:title' content='Muhammad Osama' />
-        <meta property='og:description' content='i build websites' />
-        <meta
-          property='og:image'
-          content='https://firebasestorage.googleapis.com/v0/b/uploadedbyosama.appspot.com/o/Thumbnails%2FWebsite%2Fwebsite_thumbnail.jpg?alt=media&token=b6b33f55-3de9-4417-a5b9-bfbdcc4734e2'
-        />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta property='twitter:domain' content='madebyosama.com' />
-        <meta property='twitter:url' content='https://madebyosama.com' />
-        <meta name='twitter:title' content='Muhammad Osama' />
-        <meta name='twitter:description' content='i build websites' />
-        <meta
-          name='twitter:image'
-          content='https://firebasestorage.googleapis.com/v0/b/uploadedbyosama.appspot.com/o/Thumbnails%2FWebsite%2Fwebsite_thumbnail.jpg?alt=media&token=b6b33f55-3de9-4417-a5b9-bfbdcc4734e2'
-        />
-      </head>
-      <body>
-        <div className='overlay'></div>
-        <Nav />
-        <main>{children}</main>
-        <SpeedInsights />
-        <Footer />
+    <html lang='en'>
+      <body className={`${bricolageGrotesque.variable} ${outfit.variable}`}>
+        {children}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,27 @@
-import Hero from '@/components/hero/Hero';
-import Projects from '@/components/projects/Projects';
-import Reviews from '@/components/reviews/Reviews';
-import Contact from './contact/page';
+import styles from './page.module.css';
 
-export default function HomePage() {
+export default function Page() {
   return (
-    <div>
-      <Hero />
-      <Projects />
-      <Reviews />
-      <Contact />
-    </div>
+    <main className={styles.main}>
+      <h1 className={styles.title}>Muhammad Osama </h1>
+      <p className={styles.subtitle}>Website Designer & Developer</p>
+      <div className={styles.links}>
+        <div>
+          <a href='https://wa.me/+923352522522/' target='_blank'>
+            WhatsApp
+          </a>
+        </div>
+        <div>
+          <a href='mailto:madebyosama@gmail.com' target='_blank'>
+            Email
+          </a>
+        </div>
+        <div>
+          <a href='https://instagram.com/madebyosama' target='_blank'>
+            Instagram
+          </a>
+        </div>
+      </div>
+    </main>
   );
 }
