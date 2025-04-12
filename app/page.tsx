0a29660be305f5c5fb1fe425536dc723979804d6
Muhammad Osama <madebyosama@gmail.com>
2025-04-12 15:45:02 +0500

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import styles from './page.module.css';
 export default function Page() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>Muhammad Osama </h1>
       <p className={styles.subtitle}>Website Designer & Developer</p>
+      <h1 className={styles.title}>Muhammad Osama </h1>
       <div className={styles.links}>
         <div>
           <a href='https://wa.me/+923352522522/' target='_blank'>
