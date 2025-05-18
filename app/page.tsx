5259d068b7ede53b23c431e1b29de578a616ce10
Muhammad Osama <madebyosama@gmail.com>
2025-05-18 14:34:21 +0500

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero';
+import Header from './Components/Header/Header';
+import Hero from './Components/Hero/Hero';
 import styles from './page.module.css';
 
 export default function Page() {
