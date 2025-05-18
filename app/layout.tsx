c0c8adc5a00e877f5c96ef252d31dd5acea0bb81
Muhammad Osama <madebyosama@gmail.com>
2025-05-18 12:16:06 +0500

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Bricolage_Grotesque } from 'next/font/google';
-import { Jost } from 'next/font/google';
+import { Outfit } from 'next/font/google';
 import './globals.css';
 
 const bricolageGrotesque = Bricolage_Grotesque({
@@ -8,8 +8,8 @@ const bricolageGrotesque = Bricolage_Grotesque({
   subsets: ['latin'],
 });
 
-const jost = Jost({
-  variable: '--font-jost',
+const outfit = Outfit({
+  variable: '--font-outfit',
   subsets: ['latin'],
 });
 
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${bricolageGrotesque.variable} ${jost.variable}`}>
+      <body className={`${bricolageGrotesque.variable} ${outfit.variable}`}>
         {children}
       </body>
     </html>
