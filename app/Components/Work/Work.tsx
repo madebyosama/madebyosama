7c4033fb99c682e96bdc25f48186bf38aa81206b
Muhammad Osama <madebyosama@gmail.com>
2025-05-20 11:42:31 +0500

    Updated

--- app/Components/Work/Work.tsx
+++ app/Components/Work/Work.tsx
@@ -15,6 +15,177 @@ export default function Work() {
           </div>
         </div>
       </div>
+      <div className={styles.item}>
+        <div className={styles.details}>
+          <div className={styles.detailContainer}>
+            <div className={styles.itemTitle}>MacWorld</div>
+            <div className={styles.itemDescription}>
+              An e-commerce store for user electronics.
+            </div>
+            <div className={styles.itemDetails}>
+              <div className={styles.detailTitle}>Goal:</div>
+              <div className={styles.detailDescription}>
+                Build a fast, SEO-friendly website with easy content management.
+              </div>
+            </div>
+            <div className={styles.itemDetails}>
+              <div className={styles.detailTitle}>Problem:</div>
+              <div className={styles.detailDescription}>
+                Old site was slow, cluttered, and not mobile-friendly.
+              </div>
+            </div>
+            <div className={styles.itemDetails}>
+              <div className={styles.detailTitle}>Solution:</div>
+              <div className={styles.detailDescription}>
+                Custom WordPress site with minimal plugins, clean code, and
+                responsive design.
+              </div>
+            </div>
+            <div className={styles.itemDetails}>
+              <div className={styles.detailTitle}>Result:</div>
+              <div className={styles.detailDescription}>
+                ✅ Page speed: 95+
+                <br />
+                ✅ SEO optimized
+                <br />
+                ✅ Easy to update
+                <br />
+                ✅ Mobile-first UX
+                <br />
+              </div>
+            </div>
+            <div className={styles.itemDetails}>
+              <div className={styles.detailTitle}>Link:</div>
+              <div className={styles.detailDescription}>
+                <a
+                  href='https://macworld.com'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.image}>
+          <Image src='/work1.png' width={542} height={642} alt='' />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.image}>
+          <Image src='/work2.png' width={542} height={642} alt='' />
+        </div>
+        <div className={styles.details}>
+          <div className={styles.detailContainer}>
+            <div className={styles.itemTitle}>Saeed Visa Consultants</div>
+            <div className={styles.itemDescription}>
+              An e-commerce store for user electronics.
+            </div>
+            <div className={styles.itemDetails}>
+              <div className={styles.detailTitle}>Goal:</div>
+              <div className={styles.detailDescription}>
+                Build a fast, SEO-friendly website with easy content management.
+              </div>
+            </div>
+            <div className={styles.itemDetails}>
+              <div className={styles.detailTitle}>Problem:</div>
+              <div className={styles.detailDescription}>
+                Old site was slow, cluttered, and not mobile-friendly.
+              </div>
+            </div>
+            <div className={styles.itemDetails}>
+              <div className={styles.detailTitle}>Solution:</div>
+              <div className={styles.detailDescription}>
+                Custom WordPress site with minimal plugins, clean code, and
+                responsive design.
+              </div>
+            </div>
+            <div className={styles.itemDetails}>
+              <div className={styles.detailTitle}>Result:</div>
+              <div className={styles.detailDescription}>
+                ✅ Page speed: 95+
+                <br />
+                ✅ SEO optimized
+                <br />
+                ✅ Easy to update
+                <br />
+                ✅ Mobile-first UX
+                <br />
+              </div>
+            </div>
+            <div className={styles.itemDetails}>
+              <div className={styles.detailTitle}>Link:</div>
+              <div className={styles.detailDescription}>
+                <a
+                  href='https://macworld.com'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.details}>
+          <div className={styles.detailContainer}>
+            <div className={styles.itemTitle}>GlobeWork</div>
+            <div className={styles.itemDescription}>
+              An e-commerce store for user electronics.
+            </div>
+            <div className={styles.itemDetails}>
+              <div className={styles.detailTitle}>Goal:</div>
+              <div className={styles.detailDescription}>
+                Build a fast, SEO-friendly website with easy content management.
+              </div>
+            </div>
+            <div className={styles.itemDetails}>
+              <div className={styles.detailTitle}>Problem:</div>
+              <div className={styles.detailDescription}>
+                Old site was slow, cluttered, and not mobile-friendly.
+              </div>
+            </div>
+            <div className={styles.itemDetails}>
+              <div className={styles.detailTitle}>Solution:</div>
+              <div className={styles.detailDescription}>
+                Custom WordPress site with minimal plugins, clean code, and
+                responsive design.
+              </div>
+            </div>
+            <div className={styles.itemDetails}>
+              <div className={styles.detailTitle}>Result:</div>
+              <div className={styles.detailDescription}>
+                ✅ Page speed: 95+
+                <br />
+                ✅ SEO optimized
+                <br />
+                ✅ Easy to update
+                <br />
+                ✅ Mobile-first UX
+                <br />
+              </div>
+            </div>
+            <div className={styles.itemDetails}>
+              <div className={styles.detailTitle}>Link:</div>
+              <div className={styles.detailDescription}>
+                <a
+                  href='https://macworld.com'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.image}>
+          <Image src='/work3.png' width={542} height={642} alt='' />
+        </div>
+      </div>
     </div>
   );
 }
