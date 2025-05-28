3005d59e535c71c09a7833d809d55dcdfc7c9344
Muhammad Osama <madebyosama@gmail.com>
2025-05-28 13:09:24 +0500

    Updated

--- app/Components/Section/Title/Title.tsx
+++ app/Components/Section/Title/Title.tsx
@@ -1,6 +1,6 @@
 import styles from './Title.module.css';
 
-export default function Title(props: any) {
+export default function Title(props: { title?: string; description?: string }) {
   return (
     <div className={styles.header}>
       <div className={styles.title}>{props.title || 'Title goes here.'}</div>
