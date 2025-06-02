2429c5b4357c4572a845f99bea0f6a583ca3c48e
Muhammad Osama <madebyosama@gmail.com>
2025-06-02 18:24:13 +0500

    Updated

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -1,47 +1,100 @@
 'use client';
 
-import { useState } from 'react';
+import Image from 'next/image';
 import styles from './page.module.css';
 
 type LinkItem = {
   title: string;
   url: string;
+  image: string;
 };
 
-const initialLinks: LinkItem[] = [
-  { title: 'Fiverr', url: 'https://www.fiverr.com/madebyosama' },
-  { title: 'Upwork', url: 'https://www.upwork.com/nx/find-work/best-matches' },
-  { title: 'Contra', url: 'https://contra.com/independent/home' },
-  { title: 'Zoom', url: 'https://app.zoom.us/wc/3988095546/start' },
-  { title: 'Meet', url: 'https://meet.google.com/ugw-khvs-wme' },
+const links: LinkItem[] = [
+  {
+    title: 'Fiverr',
+    url: 'https://www.fiverr.com/madebyosama',
+    image: '/fiverr.svg',
+  },
+  {
+    title: 'Upwork',
+    url: 'https://www.upwork.com/nx/find-work/best-matches',
+    image: '/upwork.svg',
+  },
+  {
+    title: 'Contra',
+    url: 'https://contra.com/independent/home',
+    image: '/contra.svg',
+  },
+  {
+    title: 'Zoom',
+    url: 'https://app.zoom.us/wc/3988095546/start',
+    image: '/zoom.svg',
+  },
+  {
+    title: 'Meet',
+    url: 'https://meet.google.com/ugw-khvs-wme',
+    image: '/meet.svg',
+  },
   {
     title: 'Linkedin',
     url: 'https://www.linkedin.com/in/madebyosama/recent-activity/all/',
+    image: '/linkedin.svg',
   },
-  { title: 'Instagram', url: 'https://www.instagram.com/madebyosama/' },
-  { title: 'X', url: 'https://twitter.com/madebyosama' },
-  { title: 'Threads', url: 'https://www.threads.net/@madebyosama' },
-  { title: 'Facebook', url: 'https://www.facebook.com/' },
-  { title: 'Youtube', url: 'https://www.youtube.com/@madebyosama' },
-  { title: 'MyMind', url: 'https://access.mymind.com/everything' },
+  {
+    title: 'Instagram',
+    url: 'https://www.instagram.com/madebyosama/',
+    image: '/instagram.svg',
+  },
+  {
+    title: 'X',
+    url: 'https://twitter.com/madebyosama',
+    image: '/x.svg',
+  },
+  {
+    title: 'Threads',
+    url: 'https://www.threads.net/@madebyosama',
+    image: '/threads.svg',
+  },
+  {
+    title: 'Facebook',
+    url: 'https://www.facebook.com/',
+    image: '/facebook.svg',
+  },
+  {
+    title: 'Youtube',
+    url: 'https://www.youtube.com/@madebyosama',
+    image: '/youtube.svg',
+  },
+  {
+    title: 'MyMind',
+    url: 'https://access.mymind.com/everything',
+    image: '/mymind.svg',
+  },
 ];
 
 export default function Links() {
-  const [links] = useState<LinkItem[]>(initialLinks);
-
   return (
     <div className={styles.page}>
       <div className={styles.links}>
         {links.map((link) => (
-          <a
+          <div
             key={link.title}
-            href={link.url}
-            target='_blank'
-            rel='noopener noreferrer'
-            className={styles.link}
+            className={styles.linkItem}
+            onClick={() => window.open(link.url, '_blank')}
           >
-            {link.title}
-          </a>
+            <div className={styles.iconContainer}>
+              <Image
+                src={link.image}
+                alt={`${link.title} icon`}
+                width={48}
+                height={48}
+                className={styles.icon}
+              />
+            </div>
+            <div className={styles.linkContainer}>
+              <p>{link.title}</p>
+            </div>
+          </div>
         ))}
       </div>
     </div>
