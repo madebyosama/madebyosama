bf4ba4cb3ed4ffca0b8123504db24c85ff9c59d9
Muhammad Osama <madebyosama@gmail.com>
2025-07-23 14:39:35 +0500

    Remove hotjar + ummami

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,28 +52,6 @@ export default function RootLayout({
     <html lang='en'>
       <head>
         <Script
-          defer
-          src='https://cloud.umami.is/script.js'
-          data-website-id='9fb6a204-44d4-404a-a889-31ef94fe7b94'
-          strategy='afterInteractive'
-        />
-        <Script
-          id='hotjar'
-          strategy='afterInteractive'
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:5125309,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          `,
-          }}
-        />
-        <Script
           src='https://www.googletagmanager.com/gtag/js?id=G-PN0VCWWC3E'
           strategy='afterInteractive'
         />
