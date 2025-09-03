889f0a0c525b2db6ccb31c5b113fda0de8b0729e
Muhammad Osama <madebyosama@gmail.com>
2025-09-03 13:35:11 +0500

    added more projects

--- data/projects.ts
+++ data/projects.ts
@@ -1,4 +1,8 @@
 import SaeedVisaConsultantsImage from '@/assets/images/projects/saeedvisaconsultants.jpg';
+import GlobeWorkNetImage from '@/assets/images/projects/globework.net.webp';
+import RahatAlMadinaParkingCarWashImage from '@/assets/images/projects/rahatalmadinaparkingcarwash.com.webp';
+import PrimosAutoRepairImage from '@/assets/images/projects/primosautorepair.com.webp';
+import BLCSolicitorsImage from '@/assets/images/projects/blcsolicitors.co.uk.webp';
 
 const Projects = [
   {
@@ -7,6 +11,38 @@ const Projects = [
     description: 'Visa / Travel',
     link: '/projects/medicosurveillance',
     url: 'https://saeedvisaconsultants.com/',
+    build: 'WordPress',
+  },
+  {
+    image: GlobeWorkNetImage,
+    title: 'Globe Work Net',
+    description: 'Visa / Travel',
+    link: '/projects/globe-work',
+    url: 'https://globework.net/',
+    build: 'WordPress',
+  },
+  {
+    image: RahatAlMadinaParkingCarWashImage,
+    title: 'Rahat Al Madina',
+    description: 'Parking / Car Wash',
+    link: '/projects/rahatalmadina',
+    url: 'https://rahatalmadinaparkingcarwash.com/',
+    build: 'WordPress',
+  },
+  {
+    image: PrimosAutoRepairImage,
+    title: 'Primos Auto Repair',
+    description: 'Car Repair',
+    link: '/projects/primosautorepair',
+    url: 'https://primosautorepair.com/',
+    build: 'WordPress',
+  },
+  {
+    image: BLCSolicitorsImage,
+    title: 'BLC Solicitors',
+    description: 'Law Firm / Lawyers',
+    link: '/projects/blc-solicitors',
+    url: 'https://www.blcsolicitors.co.uk/',
     build: 'WordPress',
   },
 ];
