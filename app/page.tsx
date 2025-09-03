f876627ec206f3202e744ee15c9e9f5fa3e2935e
Muhammad Osama <madebyosama@gmail.com>
2025-09-03 12:22:31 +0500

    development issues fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Hero from '@/components/hero/Hero';
-import Projects from '@/components/projects/Projects';
-import Reviews from '@/components/reviews/Reviews';
+import Hero from '@/components/Hero/Hero';
+import Projects from '@/components/Projects/Projects';
+import Reviews from '@/components/Reviews/Reviews';
 import Contact from './contact/page';
 
 export default function HomePage() {
