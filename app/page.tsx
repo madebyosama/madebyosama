fdc5e1c9775b2e509d0a65d1501f526090cb992f
Muhammad Osama <madebyosama@gmail.com>
2026-01-24 18:17:04 +0500

    Revert to V1

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,13 @@
-'use client';
+import Hero from '@/components/Hero/Hero'
+import Projects from '@/components/Projects/Projects'
+import Reviews from '@/components/Reviews/Reviews'
 
-import About from './Components/About/About';
-import Contact from './Components/Contact/Contact';
-import CTA from './Components/CTA/CTA';
-import FAQs from './Components/FAQs/FAQs';
-import Header from './Components/Header/Header';
-import Hero from './Components/Hero/Hero';
-import Pricing from './Components/Pricing/Pricing';
-import Problem from './Components/Problem/Problem';
-import Solution from './Components/Solution/Solution';
-import Testimonials from './Components/Testimonials/Testimonials';
-import Video from './Components/Video/Video';
-import Work from './Components/Work/Work';
-import styles from './page.module.css';
-
-export default function Page() {
+export default function HomePage() {
   return (
-    <main className={styles.main}>
-      <Header />
+    <div>
       <Hero />
-      <Video />
-      <Problem />
-      <Solution />
-      <Work />
-      <Testimonials />
-      <About />
-      <Pricing />
-      <Contact />
-      <FAQs />
-      <CTA />
-    </main>
-  );
+      <Projects />
+      <Reviews />
+    </div>
+  )
 }
