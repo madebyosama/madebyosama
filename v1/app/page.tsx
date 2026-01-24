8252dc5d6cf7af542ca5b0f095f42ef4428497c0
Muhammad Osama <madebyosama@gmail.com>
2026-01-24 18:13:11 +0500

    Revert to V2

---
+++ v1/app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from '@/components/Hero/Hero'
+import Projects from '@/components/Projects/Projects'
+import Reviews from '@/components/Reviews/Reviews'
+
+export default function HomePage() {
+  return (
+    <div>
+      <Hero />
+      <Projects />
+      <Reviews />
+    </div>
+  )
+}
