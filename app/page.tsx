6ca05375a6b2ecb119d55134f8bd0d3098412f9a
Muhammad Osama <madebyosama@gmail.com>
2026-01-25 17:29:32 +0500

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from '@/components/Hero/Hero'
 import Projects from '@/components/Projects/Projects'
 import Reviews from '@/components/Reviews/Reviews'
+import Contact from './contact/page'
 
 export default function HomePage() {
   return (
@@ -8,6 +9,7 @@ export default function HomePage() {
       <Hero />
       <Projects />
       <Reviews />
+      <Contact />
     </div>
   )
 }
