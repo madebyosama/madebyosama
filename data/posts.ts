aa2087e149f19c296d1519eca978c73e3d91daa2
Muhammad Osama <madebyosama@gmail.com>
2026-01-25 16:36:37 +0500

    Updated

--- data/posts.ts
+++ data/posts.ts
@@ -15,10 +15,10 @@ export const posts: Post[] = [
       'Life can be hard, but if you follow these steps, it can be super easy.',
     description: `
 
-<ul><li>Embrace the suck.<li>Focus on what you can control.<li>Take care of your body and mind.<li>Surround yourself with positive influences.<li>Keep learning and growing.<li>Celebrate small wins.<li>Stay patient and persistent.</ul>
+<ul><li>Throw deep, baby.</li><li>There’s blessing in the breaking.</li><li>To turn your nerves into excitement.</li><li>Cliche as it is, just don’t give up.</li><li>The world won’t always like you, but you can’t count on the world to like you.</li><li>If you are unsure about something, take a moment.</li><li>Embrace the suck.</li><li>Focus on what you can control.</li><li>Take care of your body and mind.</li><li>Surround yourself with positive influences.</li><li>Keep learning and growing.</li><li>Celebrate small wins.</li><li>Stay patient and persistent.</li></ul>
     `,
     image: '/assets/images/cover.jpg',
-    date: 'SAT, 24th JAN',
+    date: 'SAT, 25th JAN',
   },
 ]
 
