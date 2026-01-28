db616a255a567ec80b119e4e88b80ed8e73ec51e
Muhammad Osama <madebyosama@gmail.com>
2026-01-28 17:12:25 +0500

    FAQs added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from '@/components/Hero/Hero'
 import Projects from '@/components/Projects/Projects'
 import Reviews from '@/components/Reviews/Reviews'
+import FAQs from '@/components/FAQs/FAQs'
 import Contact from './contact/page'
 
 export default function HomePage() {
@@ -9,6 +10,7 @@ export default function HomePage() {
       <Hero />
       <Projects />
       <Reviews />
+      <FAQs />
       <Contact />
     </div>
   )
