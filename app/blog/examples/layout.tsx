3a9244f92cec2c842f15d982cf7e8b95d22594c5
Muhammad Osama <madebyosama@gmail.com>
2026-08-15 16:45:21 +0500

    version 3

--- app/blog/examples/layout.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '@/styles/blog.module.css'
-
-export default function NewsletterLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <div className={styles.content}>{children}</div>
-}
